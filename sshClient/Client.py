08f06810d4f07fa8d9615d40a7eb28bd3d5f0fe5
GaoYang <gaoyang1019@hotmail.com>
2020-06-29 20:34:21 +0800

    finish

--- sshClient/Client.py
+++ sshClient/Client.py
@@ -1,20 +1,21 @@
 import logging
 import sys
+from config import *
 from paramiko import AuthenticationException
 from paramiko.client import SSHClient, AutoAddPolicy
 from paramiko.ssh_exception import NoValidConnectionsError
 
 
-class MySshClient():
+class MySshClient:
     def __init__(self):
         self.ssh_client = SSHClient()
 
     # 此函数用于输入用户名密码登录主机
-    def ssh_login(self,host_ip,username,password):
+    def ssh_login(self, host_ip, username, password):
         try:
             # 设置允许连接known_hosts文件中的主机（默认连接不在known_hosts文件中的主机会拒绝连接抛出SSHException）
             self.ssh_client.set_missing_host_key_policy(AutoAddPolicy())
-            self.ssh_client.connect(host_ip,port=22,username=username,password=password)
+            self.ssh_client.connect(host_ip, port=PORT, username=username, password=password)
         except AuthenticationException:
             logging.warning('username or password error')
             return 1001
@@ -28,7 +29,7 @@ class MySshClient():
         return 1000
 
     # 此函数用于执行command参数中的命令并打印命令执行结果
-    def execute_some_command(self,command):
+    def execute_some_command(self, command):
         stdin, stdout, stderr = self.ssh_client.exec_command(command)
         print(stdout.read().decode())
 
@@ -37,20 +38,13 @@ class MySshClient():
         logging.warning('will exit host')
         self.ssh_client.close()
 
+
 if __name__ == '__main__':
-    # 远程主机IP
-    host_ip = '192.168.220.129'
-    # 远程主机用户名
-    username = 'root'
-    # 远程主机密码
-    password = 'toor'
-    # 要执行的shell命令；换成自己想要执行的命令
-    # 自己使用ssh时，命令怎么敲的command参数就怎么写
-    command = 'whoami'
+
     # 实例化
     my_ssh_client = MySshClient()
     # 登录，如果返回结果为1000，那么执行命令，然后退出
-    if my_ssh_client.ssh_login(host_ip,username,password) == 1000:
-        logging.warning(f"{host_ip}-login success, will execute command：{command}")
-        my_ssh_client.execute_some_command(command)
+    if my_ssh_client.ssh_login(HOST, USERNAME, PASSWD) == 1000:
+        logging.warning(f"{HOST}-login success, will execute command：{COMMAND}")
+        my_ssh_client.execute_some_command(COMMAND)
         my_ssh_client.ssh_logout()
